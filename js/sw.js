59f8cad159809bdc99d20cc0b30b5df4726d6c07
cieloskye <skyeldemoya@gmail.com>
2019-02-03 19:17:50 -0800

    Updated serviceWorker.

--- js/sw.js
+++ js/sw.js
@@ -5,9 +5,11 @@
  */
 
 //Service Worker
+
+//cache
 self.addEventListener('install', (event) => {
-	event.waitUntil(caches.open(cacheID).then(cache =>
-		return cache
+	event.waitUntil(caches.open('v1').then(cache =>
+		return cache(
 			.addAll([
 				'/',
 				'/index.html',
@@ -18,9 +20,35 @@ self.addEventListener('install', (event) => {
 				'/js/restaurant_info.js',
 				'/js/review.js',
 				'/js/sw.js',
-				'/js/dbhelper.js'
+				'/js/dbhelper.js',
+				'/img/1.jpg',
+				'/img/2.jpg',
+				'/img/3.jpg',
+				'/img/4.jpg',
+				'/img/5.jpg',
+				'/img/6.jpg',
+				'/img/7.jpg',
+				'/img/8.jpg',
+				'/img/9.jpg',
+				'/img/10.jpg'
 				])
 			.catch(error => {
 				console.log('Caches did not open: ' + error);
-			})));
+			}))));
 });
+
+//fetch
+
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+			if (response) {
+				return response;
+				console.log('response received');
+			}
+			else {
+				console.log('Err. event.request not found. Fetching.');
+				return fetch(event.response);
+			}
+		}))
+};
