d7deefa3cf936f4c2670372161416d84f1231db1
cieloskye <skyeldemoya@gmail.com>
2019-02-03 18:49:18 -0800

    Updated special thanks.

--- js/sw.js
+++ js/sw.js
@@ -1,5 +1,10 @@
 //sw.js
+/* Special thanks to Doug Brown for helping me get 
+ *	unstuck with his walkthrough! 
+ * 	https://www.youtube.com/watch?v=92dtrNU1GQc
+ */
+ 
 //Service Worker
-self.addEventListener('fetch', (event) => {
-	console.log(every day im offlining);
+self.addEventListener('install', (event) => {
+	event.waitUntil(caches.open(cacheID).then)
 });
