aff88c74edcd1c8797d69bc82b3d9e372226b6fb
cieloskye <skyeldemoya@gmail.com>
2019-02-05 17:10:39 -0800

    Service worker is working that cache.

--- js/sw.js
+++ js/sw.js
@@ -1,11 +1,31 @@
 //sw.js
-/* Special thanks to Doug Brown for helping me get 
- *	unstuck with his walkthrough! 
- * 	https://www.youtube.com/watch?v=92dtrNU1GQc
- */
+/* Special Thanks to:
+	developers.google.com & matthewcranford.com!
+*/
 
 //Service Worker
-
+	const cacheName = 'cache1';
+	const cacheList = [
+				'/',
+				'/index.html',
+				'/restaurant.html',
+				'/css/smStyles.css',
+				'/css/lgStyles.css',
+				'/js/main.js',
+				'/js/restaurant_info.js',
+				'/js/review.js',
+				'/js/sw.js',
+				'/js/dbhelper.js',
+				'/img/1.jpg',
+				'/img/2.jpg',
+				'/img/3.jpg',
+				'/img/4.jpg',
+				'/img/5.jpg',
+				'/img/6.jpg',
+				'/img/7.jpg',
+				'/img/8.jpg',
+				'/img/9.jpg',
+				'/img/10.jpg'];
 
 //Registration
 //https://developers.google.com/web/fundamentals/primers/service-workers/registration
@@ -13,90 +33,20 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('/sw.js');
     console.log('SW registered');
-  });
-}
-
-/* 	const cacheList = [
-				'/',
-				'/index.html',
-				'/restaurant.html',
-				'/css/smStyles.css',
-				'/css/lgStyles.css',
-				'/js/main.js',
-				'/js/restaurant_info.js',
-				'/js/review.js',
-				'/js/sw.js',
-				'/js/dbhelper.js',
-				'/img/1.jpg',
-				'/img/2.jpg',
-				'/img/3.jpg',
-				'/img/4.jpg',
-				'/img/5.jpg',
-				'/img/6.jpg',
-				'/img/7.jpg',
-				'/img/8.jpg',
-				'/img/9.jpg',
-				'/img/10.jpg'];
-
-
-	const cacheList = [
-				'/',
-				'/index.html',
-				'/restaurant.html',
-				'/css/smStyles.css',
-				'/css/lgStyles.css',
-				'/js/main.js',
-				'/js/restaurant_info.js',
-				'/js/review.js',
-				'/js/sw.js',
-				'/js/dbhelper.js',
-				'/img/1.jpg',
-				'/img/2.jpg',
-				'/img/3.jpg',
-				'/img/4.jpg',
-				'/img/5.jpg',
-				'/img/6.jpg',
-				'/img/7.jpg',
-				'/img/8.jpg',
-				'/img/9.jpg',
-				'/img/10.jpg'];
-
-
-
-
-/*
-let dbReady = false;
-
-//promise
+  })
+} else {
+	(error) => {
+		console.log('SW registration failed: ' + error);
+	}
+};
 
 
 //install cache
 self.addEventListener('install', (event) => {
-	const cacheList = [
-				'/',
-				'/index.html',
-				'/restaurant.html',
-				'/css/smStyles.css',
-				'/css/lgStyles.css',
-				'/js/main.js',
-				'/js/restaurant_info.js',
-				'/js/review.js',
-				'/js/sw.js',
-				'/js/dbhelper.js',
-				'/img/1.jpg',
-				'/img/2.jpg',
-				'/img/3.jpg',
-				'/img/4.jpg',
-				'/img/5.jpg',
-				'/img/6.jpg',
-				'/img/7.jpg',
-				'/img/8.jpg',
-				'/img/9.jpg',
-				'/img/10.jpg'];
-
-	event.waitUntil(caches.open('v1')
+	event.waitUntil(caches.open('cache1')
 		.then(cache => {
-				return cache.addAll(cacheList).catch(error => {
+				return cache.addAll(cacheList)
+				.catch(error => {
 				console.log('Caches did not open: ' + error);
 			});
 	}));
@@ -105,28 +55,26 @@ self.addEventListener('install', (event) => {
 
 //fetch cache
 self.addEventListener('fetch', (event) => {
-	let cacheRequest = event.request;
-	let cacheUrlObj = new URL(event.request.url);
-	if (event.request.url.indexOf('restaurant.html') > -1) {
-		const cacheURL = 'restaurant.html';
-		cacheRequest = new Request(cacheURL);
-	}
-*/
-/*
-	event.respondWith(
-		caches.match(event.request).then((response) => {
-			if (response) {
-				return response;
-				console.log('response received');
-			}
-			else {
-				console.log('Err. event.request not found. Fetching.');
-				return fetch(event.response);
-			}
-		});
-	});
+	    e.respondWith(
+        caches.match(e.request).then(function(response) {
 
+            if (response) {
+                return response;
+            }
+
+            else {
+                return fetch(e.request)
+                    .then(function(response) {
+                        const responseClone = response.clone();
+                        caches.open(cacheName).then(function(cache) {
+                            cache.put(e.request, responseClone);
+                        })
+                        return response;
+                    })
+                    .catch(function(err) {
+                        console.error(err);
+                    });
+            }         
+        })
+    );
 });
-
-
-*/
